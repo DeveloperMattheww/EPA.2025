dd08649a391feebdaa47c9c5d24b9839f018581c
Matheus Tavares <matheustavares1429@gmail.com>
2026-03-04 23:27:02 +0000

    Desativando Segurança

--- scripts/halloween.js
+++ scripts/halloween.js
@@ -148,16 +148,16 @@ function setupSecretShortcut() {
 
 setupSecretShortcut();
 
-document.addEventListener('keydown', function(e) {
-  if (
-    e.key === 'F12' ||
-    (e.ctrlKey && e.shiftKey && e.key === 'I') ||
-    (e.ctrlKey && e.key === 'U')
-  ) {
-    e.preventDefault();
-  }
-});
+// document.addEventListener('keydown', function(e) {
+//   if (
+//     e.key === 'F12' ||
+//     (e.ctrlKey && e.shiftKey && e.key === 'I') ||
+//     (e.ctrlKey && e.key === 'U')
+//   ) {
+//     e.preventDefault();
+//   }
+// });
 
-document.addEventListener('contextmenu', function(e) {
-  e.preventDefault();
-});
+// document.addEventListener('contextmenu', function(e) {
+//   e.preventDefault();
+// });
